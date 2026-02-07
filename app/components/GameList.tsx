4cd8f2678ea5af51ae343664bcad9160653a3e43
Nikola <bosevski.nikola1@gmail.com>
2026-02-07 19:13:14 +0100

    overall styles improvement and cards responsiveness

--- app/components/GameList.tsx
+++ app/components/GameList.tsx
@@ -18,13 +18,17 @@ if(!initialized.current){
     initialized.current = true
 }   
     return(
-       <div className="grid grid-cols-6">
+       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-y-10 place-items-center">
         {games.map(({id,image, name,provider, category})=>(
-            <div key={id} data-category={category} className="flex flex-col items-center gap-3">
-               <Image src={image} alt={name} width={200} height={150} className=""/>
-                <h2 className="bg-green-900 text-white px-10 py-5 rounded font-bold">{name}</h2>
-                <h3>{provider}</h3>
-                <button>Play</button>
+            <div key={id} data-category={category} className="group flex flex-col items-center gap-3 rounded-xl p-4 max-w-fit transition-transform hover:scale-[1.03] duration-300 ">
+               <Image src={image} alt={name} width={270} height={150} className="rounded-lg object-cover"/>
+               <div className="flex justify-between w-full items-center">
+                <div>
+                    <h2 className="w-full rounded-md  text-md font-semibold text-white">{name}</h2>
+                    <h3 className="text-sm text-slate-300 mr-auto w-fit">{provider}</h3>
+                </div>
+                <button className="w-fit rounded-md bg-[#5169D4] py-1 text-sm font-semibold text-white border-transparent transition hover:border-white focus:outline-none focus:ring-2 px-8 focus:ring-offset-2 border-2 uppercase">Play</button>
+               </div>
             </div>
         ))}
        </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import GameList from "./components/GameList";
 
 
 export default function IndexPage() {
-  return <GameList/>
+  return (
+    <div className="text-white text-center uppercase font-bold">
+      <h1 className=" my-4 text-2xl md:text-4xl">Welcome to X Casino</h1>
+      <h2 className=" mb-8 text-xl md:text-2xl">Please Choose A Game </h2>
+      <GameList/>
+    </div>
+)
 }
 
 export const metadata: Metadata = {
-  title: "Redux Toolkit",
+  title: "Casino Game Lobby",
 };
