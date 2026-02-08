2c91464ae05eb49c7e0a05fadb0f42cb190fed20
Nikola <bosevski.nikola1@gmail.com>
2026-02-08 23:07:34 +0100

    added search by name filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import GameList from "./components/GameList";
+import GameSearch from './components/GameSearch';
 
 
 export default function IndexPage() {
@@ -7,6 +8,7 @@ export default function IndexPage() {
     <div className="text-white text-center uppercase font-bold">
       <h1 className=" my-4 text-2xl md:text-4xl">Welcome to X Casino</h1>
       <h2 className=" mb-8 text-xl md:text-2xl">Please Choose A Game </h2>
+      <GameSearch/>
       <GameList/>
     </div>
 )
