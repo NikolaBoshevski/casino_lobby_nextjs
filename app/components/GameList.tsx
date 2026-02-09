05437584851d53af97db370bbb55bf9fbb992b17
Nikola <bosevski.nikola1@gmail.com>
2026-02-09 03:09:02 +0100

    added Add To Favorites feature

--- app/components/GameList.tsx
+++ app/components/GameList.tsx
@@ -5,41 +5,77 @@ import { gamesArray } from "@/lib/games"
 import { useAppDispatch, useAppSelector } from "@/lib/hooks"
 import Image from "next/image"
 import { useSearchParams } from "next/navigation"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import Favorite from "./Favorite"
 
 export default function GameList() {
+    // global variables
     const dispatch = useAppDispatch()
     const games = useAppSelector(state => state.games.items)
     const initialized = useRef(false)
     const searchParams = useSearchParams();
     const search = searchParams.get("search")?.toLowerCase() ?? ""
     const categoriesParams = searchParams.get("category")?.toLowerCase() ?? ""
-    if(search || categoriesParams ==""){
+
+    // hooks
+
+    const [favoriteIds, setFavoriteIds] = useState<string[]>([])
+    useEffect(() => {
+        const stored = localStorage.getItem("favorites")
+        if (stored) {
+            setFavoriteIds(JSON.parse(stored))
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favoriteIds))
+    }, [favoriteIds])
+
+
+    // rest of logic
+    if (search || categoriesParams == "") {
         console.warn("Search input or categoriesParams is empty")
     }
-    console.log(categoriesParams)
     const filteredGames: Game[] = games.filter(({ name, provider, category }) => {
         if ((name.toLowerCase().includes(search) || provider.toLowerCase().includes(search)) && category.toLowerCase().toString().includes(categoriesParams)) {
             return true
         }
         return false
     })
+    if (!initialized.current) {
+        dispatch(setGames(gamesArray))
+        initialized.current = true
+    }
+
+
+
+    function toggleFavorite(id: string) {
+        setFavoriteIds(prev => {
+            if (prev.includes(id)) {
+                return prev.filter(favId => favId !== id)
+            }
+            return [...prev, id]
+        })
+    }
+
+
+
+
     if (games.length == 0) {
         return <h2 className="text-black text-4xl">No games available</h2>
     }
     if (filteredGames.length == 0) {
         return <h2 className="text-black text-4xl">No games match your criteria</h2>
     }
-    if (!initialized.current) {
-        dispatch(setGames(gamesArray))
-        initialized.current = true
-    }
     return (
         <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-y-10 place-items-center">
             {filteredGames.length > 0 &&
                 filteredGames.map(({ id, name, provider, category, image }) => {
                     return (<div key={id} data-category={category} className="group flex flex-col items-center gap-3 rounded-xl p-4 max-w-fit transition-transform hover:scale-[1.03] duration-300 ">
-                        <Image src={image} alt={name} width={270} height={150} className="rounded-lg object-cover" />
+                        <div className="image-container relative">
+                            <Image src={image} alt={name} width={270} height={150} className="rounded-lg object-cover" />
+                            <Favorite filled={favoriteIds.includes(id)} onToggle={() => { toggleFavorite(id) }} />
+                        </div>
                         <div className="flex justify-between w-full items-center">
                             <div>
                                 <h2 className="w-full rounded-md  text-md font-semibold text-white">{name}</h2>
